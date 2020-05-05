85250bb2a3205be7f25c352f36193e29368f356e
Mathias Maes <mathiasmaesdev@gmail.com>
2020-05-05 11:22:03 +0200

    FIN

--- Tasks/transfertask.c
+++ Tasks/transfertask.c
@@ -30,17 +30,13 @@ void WorkerSendTask(void *pvParameters)
 {	
 	while (1)
 	{
-		if(xSemaphoreTake(mutexHandle, 10))
-		{
-			Data=0x10101010;	
-			xSemaphoreGive(mutexHandle);
-		}
+		taskENTER_CRITICAL();
+		Data=0x10101010;	
+		taskEXIT_CRITICAL();
 		
-		if(xSemaphoreTake(mutexHandle, 10))
-		{
-			Data=0x20202020;	
-			xSemaphoreGive(mutexHandle);
-		}
+		taskENTER_CRITICAL();
+		Data=0x20202020;	
+		taskEXIT_CRITICAL();
 	}
 }
 
@@ -51,15 +47,13 @@ void WorkerReceiveTask(void *pvParameters)
 	while(1)
 	{
 		time1 = portGET_RUN_TIME_COUNTER_VALUE();
-		if(xSemaphoreTake(mutexHandle, 10))
-		{
-			if (Data!=0x10101010 && Data!=0x20202020) printf ("Invalid Data value:%lx \r\n",Data);		
-			else printf ("OK\r\n");	
-			
-			xSemaphoreGive(mutexHandle);
-			
-			time2 = portGET_RUN_TIME_COUNTER_VALUE();
-		}
+		
+		taskENTER_CRITICAL();
+		if (Data!=0x10101010 && Data!=0x20202020) printf ("Invalid Data value:%lx \r\n",Data);
+		else printf ("OK\r\n");
+		taskEXIT_CRITICAL();
+		
+		time2 = portGET_RUN_TIME_COUNTER_VALUE();
 		
 		if(time1 < time2)
 		{
@@ -79,4 +73,7 @@ Dit zorgt ervoor dat er geen read en writes worden genegeerd.
 5b) Omdat af en toe het geheugen wordt gelezen terwijl dat de andere task nog bezig is met schrijven.
 Hierdoor zie je combinaties van de twee getallen verschijnen
 
+5c)
+Semaphore = 17276
+Enter/Exit Critical = 47015
 */
